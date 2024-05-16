cd71f3eb4543863f3ca4c52731c2f173564fc08f
rafli <rafli@mediainovasi.id>
2024-05-16 17:22:44 +0700

    add login with fortify

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //create user role admin
+        DB::table('users')->insert([
+            'name' => 'Rafli Andreansya',
+            'email' => 'rafli@gmail.com',
+            'phone' => '081232720821',
+            'password' => Hash::make('amaterasu'),
+            'role' => 'admin'
+        ]);
+    }
+}
